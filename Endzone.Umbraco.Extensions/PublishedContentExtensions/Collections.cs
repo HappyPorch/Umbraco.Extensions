136947a1d251e306e08bad0f606f0d39c352cc06
Filipe <filipe@endzone.io>
2017-06-02 10:22:54 +0100

    Added method to split a list into several lists of roughly the same size.

--- Endzone.Umbraco.Extensions/PublishedContentExtensions/Collections.cs
+++ Endzone.Umbraco.Extensions/PublishedContentExtensions/Collections.cs
@@ -73,5 +73,21 @@ namespace Endzone.Umbraco.Extensions.PublishedContentExtensions
             }
             return siblings;
         }
+
+        /// <summary>
+        /// Splits a list into several lists of roughly the same size
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="parts"></param>
+        /// <returns></returns>
+        public static IEnumerable<IEnumerable<T>> Split<T>(this IEnumerable<T> list, int parts)
+        {
+            int i = 0;
+            var splits = from item in list
+                         group item by i++ % parts into part
+                         select part.AsEnumerable();
+            return splits;
+        }
     }
 }
