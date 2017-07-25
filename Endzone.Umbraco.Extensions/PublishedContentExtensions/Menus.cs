c9ca17d38dcb717e3791b4fe45c79af08c78a2a3
Filipe <filipe@endzone.io>
2017-07-25 11:57:04 +0100

    The menu now highlights the current page and parent.

--- Endzone.Umbraco.Extensions/PublishedContentExtensions/Menus.cs
+++ Endzone.Umbraco.Extensions/PublishedContentExtensions/Menus.cs
@@ -84,6 +84,8 @@ namespace Endzone.Umbraco.Extensions.PublishedContentExtensions
         {
             var mainMenu = content.GetNestedContent(property);
             var umbracoHelper = new UmbracoHelper(UmbracoContext.Current);
+            var umbracoContext = umbracoHelper.UmbracoContext;
+            var currentPage = umbracoContext.ContentCache.GetById(umbracoContext.PageId.Value);
             var markup = new StringBuilder();
             foreach (var item in mainMenu)
             {
@@ -98,7 +100,7 @@ namespace Endzone.Umbraco.Extensions.PublishedContentExtensions
                         continue;
                     }
                     var linkText = useCustomLinkText ? item.GetPropertyValue<string>("linkText") : link.Name;
-                    markup.Append($"<li class=\"{(content.Id == link.Id ? "current" : null)}\">");
+                    markup.Append($"<li class=\"{(currentPage.IsDescendantOrSelf(linkContent) ? "current" : null)}\">");
                     markup.Append($"<a target=\"{link.Target}\" href=\"{link.Url}\">{linkText}</a>");
                     var showLinksChildren = showChildren;
                     if (item.HasValue("showLinksChildren"))
@@ -121,11 +123,11 @@ namespace Endzone.Umbraco.Extensions.PublishedContentExtensions
                         {
                             if (addParentToSubMenu)
                             {
-                                markup.Append($"<li><a target=\"{ link.Target}\" href=\"{ link.Url}\">{link.Name}</a></li>");
+                                markup.Append($"<li><a target=\"{link.Target}\" href=\"{link.Url}\">{link.Name}</a></li>");
                             }
                             foreach (var child in linkContent.ChildrenVisible())
                             {
-                                markup.Append($"<li><a href=\"{child.Url}\">{child.Name}</a></li>");
+                                markup.Append($"<li class=\"{(currentPage.IsDescendantOrSelf(child) ? "current" : null)}\"><a href=\"{child.Url}\">{child.Name}</a></li>");
                             }
                         }
                         markup.Append("</ul>");
