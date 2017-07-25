d200ca1a51c12dd04e00abf983fc271b2cc49cb3
Filipe <filipe@endzone.io>
2017-07-25 10:53:44 +0100

    Allows menus to list tags instead of children.

--- Endzone.Umbraco.Extensions/PublishedContentExtensions/Menus.cs
+++ Endzone.Umbraco.Extensions/PublishedContentExtensions/Menus.cs
@@ -105,21 +105,28 @@ namespace Endzone.Umbraco.Extensions.PublishedContentExtensions
                     {
                         showLinksChildren = item.GetPropertyValue<string>("showLinksChildren").ToLower() == "yes";
                     }
-                    if (showLinksChildren && linkContent.Children.Any())
+                    if (showLinksChildren)
                     {
                         markup.Append($"<ul class=\"{ulInnerClass}\">");
-                        if (addParentToSubMenu)
+                        if (item.HasValue("tagsProperty"))
                         {
-                            markup.Append($"<li><a target=\"{ link.Target}\" href=\"{ link.Url}\">{link.Name}</a></li>");
-                        }
-                        foreach (var child in linkContent.ChildrenVisible())
-                        {
-                            var childIsHidden = child.GetPropertyValue<bool>("umbracoNaviHide");
-                            if (childIsHidden)
+                            var tagsProperty = item.GetPropertyValue<string>("tagsProperty");
+                            var tags = linkContent.ChildrenVisible().GetAllTags(tagsProperty);
+                            foreach (var tag in tags)
                             {
-                                continue;
+                                markup.Append($"<li><a href=\"{linkContent.Url}#{tag.SanitizeTagName()}\">{tag}</a></li>");
                             }
-                            markup.Append($"<li><a href=\"{child.Url}\">{child.Name}</a></li>");
+                        }
+                        else if (linkContent.ChildrenVisible().Any())
+                        {
+                            if (addParentToSubMenu)
+                            {
+                                markup.Append($"<li><a target=\"{ link.Target}\" href=\"{ link.Url}\">{link.Name}</a></li>");
+                            }
+                            foreach (var child in linkContent.ChildrenVisible())
+                            {
+                                markup.Append($"<li><a href=\"{child.Url}\">{child.Name}</a></li>");
+                            }
                         }
                         markup.Append("</ul>");
                     }
@@ -128,7 +135,6 @@ namespace Endzone.Umbraco.Extensions.PublishedContentExtensions
                 else if (link.Type == LinkType.External)
                 {
                     markup.Append($"<li><a href=\"{link.Url}\">{link.Name}</a></li>");
-
                 }
             }
             return new HtmlString(markup.ToString());
