b145347b5abe06940f454176b99b6319ecffa5cb
Filipe <filipe@endzone.io>
2017-08-11 13:54:01 +0100

    Fixed the GetWebsiteSettingsMethods to work for pages inside the "Website Components" folder.

--- Endzone.Umbraco.Extensions/PublishedContentExtensions/Configuration.cs
+++ Endzone.Umbraco.Extensions/PublishedContentExtensions/Configuration.cs
@@ -16,13 +16,25 @@ namespace Endzone.Umbraco.Extensions.PublishedContentExtensions
         public static IPublishedContent GetWebsiteSettings(this IPublishedContent content)
         {
             var umbracoHelper = new UmbracoHelper(UmbracoContext.Current);
+            var umbracoContext = umbracoHelper.UmbracoContext;
             var homepage = content.AncestorOrSelf(1);
             //If it's a nested content element or the 'website settings' node, it has no knowledge of the home page and we need to get it some other way.
-            if (content.Level == 0 || content.DocumentTypeAlias.ToLower() == "websitesettings")
+            if (!homepage.HasProperty("websiteSettings"))
             {
-                var umbracoContext = umbracoHelper.UmbracoContext;
                 var currentPage = umbracoContext.ContentCache.GetById(umbracoContext.PageId.Value);
                 homepage = currentPage.AncestorOrSelf(1);
+            }
+            //It can be a page inside the website components folder rendering by itself.
+            if (!homepage.HasProperty("websiteSettings"))
+            {
+                var currentPage = umbracoContext.ContentCache.GetById(umbracoContext.PageId.Value);
+                // the website components folder may not be at the root such as when there are multiple websites.
+                var websiteComponents = currentPage.AncestorsOrSelf().FirstOrDefault(c => c.DocumentTypeAlias.ToLower().Contains("websitecomponents"));
+                if (websiteComponents != null)
+                {
+                    var rootNodes = umbracoHelper.TypedContentAtRoot();
+                    homepage = rootNodes.FirstOrDefault(c => c.HasValue("websiteComponents") && c.GetLink("websiteComponents").Id == websiteComponents.Id);
+                }
             }
             var link = homepage.GetLink("websiteSettings");
             return umbracoHelper.TypedContent(link.Id);
