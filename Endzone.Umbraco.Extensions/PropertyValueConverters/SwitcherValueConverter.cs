2f7af365d8ab77c0b3d9e74f0351e061b977311b
Tom van Enckevort <tom@endzone.io>
2017-10-24 10:47:08 +0100

    Added property value converter for Switcher and UmbracoContextHelper

---
+++ Endzone.Umbraco.Extensions/PropertyValueConverters/SwitcherValueConverter.cs
@@ -0,0 +1,41 @@
+﻿using Umbraco.Core;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Core.PropertyEditors;
+
+namespace Endzone.Umbraco.Extensions.PropertyValueConverters
+{
+    /// <summary>
+    /// PropertyValueConverter which converts the value stored for a Switcher data type to a boolean.
+    /// </summary>
+    [PropertyValueType(typeof(bool))]
+    [PropertyValueCache(PropertyCacheValue.All, PropertyCacheLevel.ContentCache)]
+    public class SwitcherValueConverter : IPropertyValueConverter
+    {
+        public bool IsConverter(PublishedPropertyType propertyType)
+        {
+            return propertyType.PropertyEditorAlias.Equals("Our.Umbraco.Switcher");
+        }
+
+        public object ConvertDataToSource(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            var attemptConvertBool = source.TryConvertTo<bool>();
+
+            if (attemptConvertBool.Success)
+            {
+                return attemptConvertBool.Result;
+            }
+
+            return null;
+        }
+
+        public object ConvertSourceToObject(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            return source;
+        }
+
+        public object ConvertSourceToXPath(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            return source?.ToString();
+        }
+    }
+}
